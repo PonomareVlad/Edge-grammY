7d0e49981df3c17706bb2a715dc95b8bd2b76ea7
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2023-04-17 22:03:00 +0500

    Added methods for parsing hostname and build urls

--- index.mjs
+++ index.mjs
@@ -1,7 +1,13 @@
 import {promisify} from "util";
 import {webhookCallback} from "grammy";
 
-const wait = promisify((a, f) => setTimeout(f, a));
+export const wait = promisify((a, f) => setTimeout(f, a));
+
+export const getURL = (path = "api/index", headers = {}, proxy = "") => {
+    return new URL(`${proxy}https://${getHost(headers)}/${path}`);
+}
+
+export const getHost = (headers = {}, header = "x-forwarded-host") => headers?.get(header) || process.env.VERCEL_URL;
 
 export const webhookStream = (bot, adapter = "std/http", onTimeout = "return", timeoutMilliseconds = 180_000) => {
     const callback = webhookCallback(bot, adapter, onTimeout, timeoutMilliseconds);
