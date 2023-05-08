660b0b6ca77ac861cf0e3607f3f44290dd168220
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2023-05-08 23:28:31 +0500

    Added setWebhook and JSON response methods

--- index.mjs
+++ index.mjs
@@ -3,11 +3,15 @@ import {webhookCallback} from "grammy";
 
 export const wait = promisify((a, f) => setTimeout(f, a));
 
+export const json = (...args) => new Response(JSON.stringify(...args));
+
 export const getURL = (path = "api/index", headers = {}, proxy = "") => {
-    return new URL(`${proxy}https://${getHost(headers)}/${path}`);
+    return new URL(`${proxy}https://${getHost(headers)}/${path}`).href;
 }
 
 export const getHost = (headers = {}, header = "x-forwarded-host") => headers?.get(header) || process.env.VERCEL_URL;
+
+export const setWebhook = (bot, path) => async ({headers}) => json(await bot.api.setWebhook(getURL(path, headers)));
 
 export const webhookStream = (bot, adapter = "std/http", onTimeout = "throw", timeoutMilliseconds = 55_000) => {
     const callback = webhookCallback(bot, adapter, onTimeout, timeoutMilliseconds);
@@ -22,5 +26,3 @@ export const webhookStream = (bot, adapter = "std/http", onTimeout = "throw", ti
         return new Response(stream);
     }
 }
-
-export default webhookStream;
